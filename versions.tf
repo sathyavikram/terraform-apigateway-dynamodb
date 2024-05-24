f037a61aa98c3c1e7d750829598fe37ebc726222
Sathya Vikram <2120744+sathyavikram@users.noreply.github.com>
2024-05-24 07:58:06 -0400

    Step 2: Setup AWS and link it to terraform

--- versions.tf
+++ versions.tf
@@ -9,4 +9,15 @@ terraform {
       tags = ["dev"]
     }
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.51.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
